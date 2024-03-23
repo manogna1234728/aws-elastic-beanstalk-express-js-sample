ac3a23da520958bbdbc8c4bec45e838b6b611fb5
Manogna Sabbani <manognasabbani@Manognas-MacBook-Air.local>
2024-03-23 10:55:01 -0500

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('This is my first time trying a project with AWS entirely on my own and I am so excited about this'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
